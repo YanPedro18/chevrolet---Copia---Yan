83d10d4aca76f881211065448b90e176b4a1d4c9
Yan Pedro <leitebr14@gmail.com>
2020-10-26 03:17:59 -0300

    Modific!
    Add js end modific responsive add icon whatsapp

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,21 @@ function menuOpen() {
 
 
 icon.addEventListener('click', menuOpen);
+
+const linksInternos = document.querySelectorAll('.menu a[href^="#"');
+
+function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    const topo = section.offsetTop;
+    window.scrollTo({
+        top: topo,
+        behavior: "smooth",
+    });
+}
+
+
+linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+});
